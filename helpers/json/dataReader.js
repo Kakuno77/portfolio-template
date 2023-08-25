2d377c27a21a4b3713a6b2b847ffefcb0e2f7a33
rathesh77 <rathesh.charles@outlook.fr>
2023-08-25 18:53:50 +0200

    initial commit

---
+++ helpers/json/dataReader.js
@@ -0,0 +1,5 @@
+const fs = require('fs')
+
+module.exports.readDataFromJson = (filepath) => {
+    return JSON.parse(fs.readFileSync(filepath, 'utf8'))
+}
---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.use(express.static('public'));
+
+const { readDataFromJson } = require('./helpers/json/dataReader')
+const experiences = readDataFromJson('./json/experiences.json')
+const whoami = readDataFromJson('./json/whoami.json')
+const projects = readDataFromJson('./json/projects.json')
+const profilePicture = readDataFromJson('./json/profile-picture.json')
+const skills = readDataFromJson('./json/skills.json')
+
+app.get('/', function (_, res) {
+    const sections = {
+        profile_picture: profilePicture,
+        whoami,
+        experiences,
+        projects,
+        skills
+    }
+    console.log(sections)
+    res.render('index', { sections });
+});
+
+app.listen(8080);
+console.log('8080 is the magic port');
